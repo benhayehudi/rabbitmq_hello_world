c16e01082baed146a708f09b1d7775f3dd4cbe61
benhayehudi <bengreenberg@gmail.com>
2017-12-14 11:56:14 -0500

    created a hello world publisher

---
+++ send.js
@@ -0,0 +1,22 @@
+// require the amqp library
+const amqp = require('amqplib/callback_api');
+
+// connect to RabbitMQ server
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    // define two variables: q == queue & msg = message
+    let q = "hello";
+    let msg = "Hello World!";
+
+    // set up the queue in the channel
+    ch.assertQueue(q, {durable: false});
+    // send to queue the message
+    ch.sendToQueue(q, new Buffer(msg));
+
+    // show us the successful sending of msg
+    console.log(" [x] Sent %s", msg);
+  });
+
+  // after 500ms close connection & exit the process
+  setTimeout(function() {conn.close(); process.exit(0) }, 500)
+});
